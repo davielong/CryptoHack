4b764ecfab36c34359c7a44c0a529fbfaa4b3010
davielong <b00131083@mytudublin.ie>
2021-01-09 20:13:16 +0000

    update after laptop loss

---
+++ BlockCiphers/Blockstarter_MODE.py
@@ -0,0 +1,1 @@
+to solve the block cipher statyer mode chjallenge I simply generated the encrypted flag then decrypted it to a hex value then decoded that into an ascii string using the web interface
---
+++ DH/DH2.py
@@ -0,0 +1,15 @@
+p =28151
+g = 2
+
+found = False
+
+while not found:
+    for n in range(2,p):
+    #g ^ n mod p1
+
+        if pow(g,n ,p) == g:
+            break
+        if n == p-2:
+            print(g)
+            found = True
+    g+=1
